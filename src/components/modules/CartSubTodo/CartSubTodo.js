349ab3c2c8b9f1e3434a56995c12bd9f73853635
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-12-23 14:01:36 +0330

    fixed style cartSubNote and cartSubTodo

--- src/components/modules/CartSubTodo/CartSubTodo.js
+++ src/components/modules/CartSubTodo/CartSubTodo.js
@@ -5,9 +5,9 @@ function CartSubTodo() {
     <div className="block border p-2 rounded-md border-background-dark mt-4 dark:border-font-100">
     <span className="block font-danaBold text-xl text-font-100"> اموزش Bash sciprt</span>
     <div className="flex items-start justify-end  gap-3 mt-2">
-        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-secondary-100">مشاهده</button>
-        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-red-600">حذف</button>
-        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-primary-100">ویرایش</button>
+        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-secondary-100 dark:hover:text-secondary-100">مشاهده</button>
+        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-red-600 dark:hover:text-red-600">حذف</button>
+        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-primary-100 dark:hover:text-primary-100">ویرایش</button>
     </div>
 </div>
   )
