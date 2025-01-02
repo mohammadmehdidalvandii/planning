4cde85634017d5c1dcf5d55cce150bb8088143f8
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-01-02 16:16:27 +0330

    fixed buttons CartSubNote

--- src/components/modules/CartSubNote/CartSubNote.js
+++ src/components/modules/CartSubNote/CartSubNote.js
@@ -1,16 +1,81 @@
-import React from 'react'
+"use client";
+import EditNote from "@/components/template/noteList/EditNote/EditNote";
+import useNoteStore from "@/zustand/useNoteStore";
+import React, { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
-function CartSubNote() {
+function CartSubNote({ id, subject , text }) {
+  const {removeNote} = useNoteStore()
+  const [showTextNote, setShowTextNote] = useState("");
+  const [showUpdata , setShowUpdate] = useState(false);
+  const handlerEditNote =()=>{
+    setShowUpdate(true)
+  }
+
+  const handlerShowText = () => {
+    setShowTextNote(true);
+  };
+
+  const handlerExitAddNote =()=>{
+    setShowTextNote(false)
+    setShowUpdate(false)
+  }
+
   return (
     <div className="block border p-2 rounded-md border-background-dark mt-4 dark:border-font-100">
-    <span className="block font-danaBold text-xl text-font-100">نحوه ادیت کردن ویدیو با این شات</span>
-    <div className="flex items-start justify-end  gap-3 mt-2">
-        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-secondary-100 dark:hover:text-secondary-100">مشاهده</button>
-        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-red-600 dark:hover:text-red-600">حذف</button>
-        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-primary-100 dark:hover:text-primary-100">ویرایش</button>
+      <span className="block font-danaBold text-xl text-font-100">
+        {subject}
+      </span>
+      <div className="flex items-start justify-end  gap-3 mt-2">
+        <button
+          className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-secondary-100 dark:hover:text-secondary-100"
+          onClick={handlerShowText}
+        >
+          مشاهده
+        </button>
+        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-red-600 dark:hover:text-red-600"
+          onClick={(id)=>{
+            removeNote(id)
+          }}
+        >
+          حذف
+        </button>
+        <button className="flex text-font-default dark:text-font-100 ease-linear duration-300 hover:text-primary-100 dark:hover:text-primary-100"
+          onClick={handlerEditNote}
+        >
+          ویرایش
+        </button>
+      </div>
+
+      {showTextNote && (
+        <div className="shadow right-0">
+          <button
+            className="btn_red mt-1 mr-1 w-[70px]"
+            onClick={handlerExitAddNote}
+          >
+            <FaTimes />
+          </button>
+          <div className="box max-w-[50%] mx-auto mt-12">
+            <p className="break-words w-full text-wrap font-danaMedium text-xl text-font-default dark:text-font-200">
+              {text}
+            </p>
+          </div>
+        </div>
+      )}
+      {showUpdata && (
+         <div className="shadow right-0" 
+         >
+                <button className="btn_red mt-1 mr-1 w-[70px]" 
+                     onClick={handlerExitAddNote}
+                 ><FaTimes/></button>
+                 <EditNote
+                  id={id}
+                 />  
+                 
+         </div>
+      )}
     </div>
-</div>
-  )
+  );
 }
 
-export default CartSubNote
+export default CartSubNote;
--- src/components/template/panel/LastNote/LastNote.js
+++ src/components/template/panel/LastNote/LastNote.js
@@ -1,14 +1,34 @@
 import CartSubNote from '@/components/modules/CartSubNote/CartSubNote'
+import connectDB from '@/config/db'
 import React from 'react'
+import NoteModel from '@/models/Note';
 
-function LastNote() {
+
+async function LastNote() {
+  await connectDB();
+
+  const notes = await NoteModel.find({})
+
+
   return (
     <section className="block w-full">
         <div className="box">
             <h6 className="subTitle">آخرین یاد داشت ها </h6>
-           <CartSubNote/>
-           <CartSubNote/>
-           <CartSubNote/>
+            {
+              notes.length > 0 ? (
+                notes.reverse().slice(0,3).map(note=>(
+                  <CartSubNote key={note._id}
+                  id={note._id}
+                  subject={note.subject}
+                  text={note.text}
+                  />
+                ))
+              ):(
+                <p colSpan="4" className="px-2 py-3 text-center font-danaBlack text-2xl text-red-600">یادداشت وجود ندارد</p>
+
+              )
+            }
+        
         </div>
     </section>
   )
